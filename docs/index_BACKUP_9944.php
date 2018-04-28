cbf8abb0fc8281d4bcd4384967a53b7f97edd684
nikita <super.tyler33@live.com>
2018-04-28 16:43:19 +0300

    init

---
+++ docs/index_BACKUP_9944.php
@@ -0,0 +1,8 @@
+<?php
+   echo "Hello Git!";
+   echo "Wha`sup!";
+<<<<<<< HEAD
+   echo "Hello, new branch!";
+=======
+   echo "Branch master";
+>>>>>>> master
---
+++ docs/index_BASE_8688.php
@@ -0,0 +1,3 @@
+<?php
+   echo "Hello Git!";
+   echo "Wha`sup!";
